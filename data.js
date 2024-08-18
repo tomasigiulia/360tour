07dcb0f8b9f4a15815f5770c7a4165472ec5ffca
tomasigiulia <tomasigiulia@fondazionemcr.it>
2024-08-18 15:09:38 +0200

    upload

---
+++ data.js
@@ -0,0 +1,59 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-rocolo",
+      "name": "rocolo",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "yaw": -2.654097514459398,
+        "pitch": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.8311458840059203,
+          "pitch": -0.0021943652544820225,
+          "title": "casa di riposo",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.936838119292357,
+          "pitch": -0.015440115302929769,
+          "title": "Title",
+          "text": "<div>prova</div>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
